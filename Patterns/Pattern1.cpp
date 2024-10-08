30bdd328d921d86321627221d74e6662a48e7e58
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-08 22:20:57 +0530

    Added pattern logic

--- Patterns/Pattern1.cpp
+++ Patterns/Pattern1.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main(){
-    
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    for(int i=0;i<n;i++){
+        for(int j=1;j<=n;j++){
+            cout << j;
+        }
+        cout << endl;
+    }
 }
