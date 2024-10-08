8e03e150027ba3b6ef3cd9d9343bc8e98cb4cd73
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-08 22:53:13 +0530

    Added spaces

--- Patterns/Pattern3.cpp
+++ Patterns/Pattern3.cpp
@@ -7,7 +7,7 @@ int main() {
     cin >> n;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            cout << num;
+            cout << num << " ";
             num++;
         }
         cout << endl;
