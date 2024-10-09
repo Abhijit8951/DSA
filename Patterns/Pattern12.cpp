0c8a2391455e81af114d038d0b508d0b14d9fe38
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-09 10:54:09 +0530

    Addes logics

--- Patterns/Pattern12.cpp
+++ Patterns/Pattern12.cpp
@@ -3,7 +3,27 @@
 using namespace std;
 
 int main(){
-    
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    for(int i=0;i<n;i++){
+        //spaces: n-i-1
+        for(int j=0;j<n-i-1;j++){
+            cout << " ";
+        }
+
+        //nums1: i+1 times
+        for(int j=1;j<=i+1;j++){
+            cout << j;
+        }
+
+        //nums2
+        for(int j=i;j>=1;j--){
+            cout << j;
+        }
+
+        cout << endl;
+    }
 
     return 0;
 }
