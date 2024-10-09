c598522623c6620450db443f34f40f38e327e485
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-09 07:17:05 +0530

    Added program logic

--- Patterns/Pattern7.cpp
+++ Patterns/Pattern7.cpp
@@ -3,7 +3,17 @@ using namespace std;
 
 
 int main(){
-
+    int n;
+    char ch = 'A';
+    cout << "Enter a number: ";
+    cin >> n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            cout << ch << " ";
+        }
+        ch++;
+        cout << endl;
+    }
 
 
     return 0;
