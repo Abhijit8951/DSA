0e9a7d57075a53c4585e1e2231d688aed66f3398
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-09 11:22:12 +0530

    Added logics

--- Patterns/Pattern13.cpp
+++ Patterns/Pattern13.cpp
@@ -3,7 +3,48 @@
 using namespace std;
 
 int main(){
-    
+    int n=4;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    //top-part
+    for(int i=0;i<n;i++){
+        //spaces
+        for(int j=0;j<n-i-1;j++){
+            cout << " ";
+        }
+
+        cout << "*";
+
+        if(i != 0){
+            //spaces
+            for(int j=0;j<2*i-1;j++){
+                cout << " ";
+            }
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    //Bottom-part
+    for(int i=0;i<n-1;i++){
+        //spaces
+        for(int j=0;j<i+1;j++){
+            cout << " ";
+        }
+
+        cout << "*";
+
+        if(i != n-2){
+            //spaces
+            for(int j=0;j<2*(n-i)-5;j++){
+                cout << " ";
+            }
+            cout << "*";
+        }
+
+        cout << endl;
+    }
 
     return 0;
 }
