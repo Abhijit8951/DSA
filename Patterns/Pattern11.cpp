5e5fe825f8df3f0fbcea142a8590cb12343e3ef6
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-09 10:03:43 +0530

    Added logic

--- Patterns/Pattern11.cpp
+++ Patterns/Pattern11.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    for(int i=0;i<n;i++){
+
+        //spaces
+        for(int j=0;j<i;j++){
+            cout << " ";
+        }
+        //numbers
+        for(int j=0;j<n-i;j++){
+            cout << i+1;
+        }
+        cout << endl;
+    }
 
 
     return 0;
