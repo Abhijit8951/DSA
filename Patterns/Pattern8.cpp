316ed58a68c95364f3cdc926a3c6ca4bcdef8f20
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-09 07:24:32 +0530

    Added program logic

--- Patterns/Pattern8.cpp
+++ Patterns/Pattern8.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main(){
-    
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    for(int i=0;i<n;i++){
+        int num = 1;
+        for(int j=0;j<i+1;j++){
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
 
     return 0;
 }
