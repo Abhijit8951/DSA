2e7d2009bbf11e4ca3dd6eee241d811d036e1feb
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-09 07:32:12 +0530

    Added logic

--- Patterns/Pattern9.cpp
+++ Patterns/Pattern9.cpp
@@ -3,7 +3,15 @@
 using namespace std;
 
 int main(){
-
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j>=1;j--){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
