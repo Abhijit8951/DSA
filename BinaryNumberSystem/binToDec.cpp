65446f3d98aa3310b84675bac0e0ed4cfc7ab3d1
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-10 09:04:41 +0530

    Added logic

--- BinaryNumberSystem/binToDec.cpp
+++ BinaryNumberSystem/binToDec.cpp
@@ -2,8 +2,22 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int binToDecimal(int n){
+    int ans=0,pow=1;
+    while(n>0){
+        int rem = n%10;
+        ans += (rem*pow);
+        n /= 10;
+        pow *= 2;
+    }
+    return ans;
+}
 
+int main(){
+    int n;
+    cout << "Enter a binary number: ";
+    cin >> n;
+    cout << binToDecimal(n) << endl;
 
     return 0;
 }
