0f26c092b61fd363b55e04d16cc9bbb7da158abe
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-10 08:56:28 +0530

    added logic

--- BinaryNumberSystem/decToBin.cpp
+++ BinaryNumberSystem/decToBin.cpp
@@ -2,6 +2,16 @@
 #include <iostream>
 using namespace std;
 
+int decToBinary(int n){
+    int ans=0, pow=1;
+    while(n>0){
+        int rem=n%2;
+        ans+=(rem*pow);
+        n /= 2;
+        pow *= 10;
+    }
+    return ans;
+}
 
 int main() {
     int n;
