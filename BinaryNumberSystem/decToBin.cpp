9b8074cabbd7de182efc80e1fff200c1652fd55f
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-10 08:53:54 +0530

    Added initials

---
+++ BinaryNumberSystem/decToBin.cpp
@@ -0,0 +1,13 @@
+//Decimal to Binary
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int n;
+    cout << "enter a decimal number: ";
+    cin >> n;
+    cout << decToBinary(n) << endl;
+
+    return 0;
+}
