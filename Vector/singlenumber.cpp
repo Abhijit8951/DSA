a8a15d22375ece035b6be91f1418693c735d2f6a
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-11 11:23:28 +0530

    Added logic

--- Vector/singlenumber.cpp
+++ Vector/singlenumber.cpp
@@ -4,7 +4,13 @@
 using namespace std;
 
 int main(){
+    vector<int> vec = {4, 1, 2, 1, 2};
+    int ans = 0;
+    for(int val : vec){
+        ans = ans^val;
+    }
 
+    cout << "Th eunique element is: " << ans << endl;
 
     return 0;
 }
