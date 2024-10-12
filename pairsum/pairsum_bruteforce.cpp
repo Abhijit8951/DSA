1369b863a7eba0ef87013feb6a6a65a6e225a79e
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-12 08:05:12 +0530

    Added initials

--- pairsum/pairsum_bruteforce.cpp
+++ pairsum/pairsum_bruteforce.cpp
@@ -2,7 +2,27 @@
 #include <vector>
 using namespace std;
 
+vector<int> pairsum(vector<int> nums, int target){
+    vector<int> ans;
+    int size=nums.size();
+    for(int i=0;i<size;i++){
+        for(int j=i+1;j<size;j++){
+            if(nums[i]+nums[j] == target){
+                ans.push_back(nums[i]);
+                ans.push_back(nums[j]);
+                return ans;
+            }
+        }
+    }
+    return ans;
+}
+
 int main(){
+    vector<int> nums = {2, 9, 11, 4, 7};
+    int target = 13;
+
+    vector<int> ans = pairsum(nums, target);
+    cout << ans[0] << " , " << ans[1] << endl;
 
 
     return 0;
