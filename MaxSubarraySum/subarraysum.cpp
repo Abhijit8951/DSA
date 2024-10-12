f57de50b435e64b2b33bf86600340224f9cd22be
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-12 06:03:07 +0530

    Added initials

--- MaxSubarraySum/subarraysum.cpp
+++ MaxSubarraySum/subarraysum.cpp
@@ -4,6 +4,18 @@
 using namespace std;
 
 int main(){
+    int n = 5;
+    int arr[5] = {1, 2, 7, 4, -8};
+    int maxSum = INT_MIN;
+
+    for(int st=0;st<n;st++){
+        int currSum = 0;
+        for(int end=st;end<n;end++){
+            currSum+=arr[end];
+            maxSum=max(currSum, maxSum);
+        }
+    }
+    cout << "Max subarray sum: " << maxSum << endl;
 
 
     return 0;
