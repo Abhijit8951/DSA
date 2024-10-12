400758a87123208e32ace99d8c08323ee90bf1a4
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-12 06:25:00 +0530

    Added initials

--- MaxSubarraySum/kadaneAlgo.cpp
+++ MaxSubarraySum/kadaneAlgo.cpp
@@ -4,6 +4,17 @@
 using namespace std;
 
 int main(){
+    int n = 8;
+    int arr[8] = {3, 1, -5, 3, 8, -1, 3, -4};
+    int currSum = 0,maxSum = INT_MIN;
+    for(int i=0;i<n;i++){
+        currSum+=arr[i];
+        maxSum=max(currSum, maxSum);
+        if(currSum < 0){
+            currSum=0;
+        }
+    }
+    cout << "Maximum subarray sum: " << maxSum << endl;
 
 
     return 0;
