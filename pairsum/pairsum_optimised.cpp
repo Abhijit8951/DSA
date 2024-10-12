bcbe837b06ecc1a70c59e5a764ef5040c035c9e1
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-12 08:30:49 +0530

    Added optimised logic

--- pairsum/pairsum_optimised.cpp
+++ pairsum/pairsum_optimised.cpp
@@ -2,8 +2,32 @@
 #include <vector>
 using namespace std;
 
-int main(){
+vector<int> pairsum(vector<int> nums, int target){
+    vector<int> ans;
+    int size=nums.size();
+    int i=0, j=size-1;
+    while(i<j){
+        int pairsum = nums[i]+nums[j];
+        if(pairsum < target){
+            i++;
+        } else if(pairsum > target){
+            j--;
+        } else{
+            ans.push_back(nums[i]);
+            ans.push_back(nums[j]);
+            return ans;
+        }
+    }
 
+    return ans;
+}
+
+int main(){
+    vector<int> nums = {2, 9, 11, 4, 7};
+    int target = 13;
+
+    vector<int> ans = pairsum(nums, target);
+    cout << ans[0] << " , " << ans[1] << endl;
 
     return 0;
 }
