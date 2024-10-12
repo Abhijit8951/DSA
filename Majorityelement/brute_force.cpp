c12bb57487990e25e3ff54f1f89cb2ed3eb73c9e
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-13 00:46:20 +0530

    Added logic

--- Majorityelement/brute_force.cpp
+++ Majorityelement/brute_force.cpp
@@ -4,5 +4,20 @@
 using namespace std;
 
 int main(){
+    vector<int> vec = {2, 1, 2, 1, 1};
+    int n = vec.size();
 
+    for(int val : vec){
+        int freq = 0;
+        for(int el : vec){
+            if(el == val){
+                freq++;
+            }
+        }
+        if(freq>n/2){
+            cout << val << endl;
+            return 0;
+        }
+    }
+    return -1;
 }
