a94efdfd27fd2013dd39b1334bede221369b308a
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-13 07:23:54 +0530

    Added logic

--- Majorityelement/optimised.cpp
+++ Majorityelement/optimised.cpp
@@ -4,7 +4,23 @@
 using namespace std;
 
 int main(){
-    
+    vector<int> nums = {1, 2, 1, 1, 2};
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+
+    int freq=1, ans=nums[0];
+    for(int i=1; i<n; i++){
+        if(nums[i]==nums[i-1]){
+            freq++;
+        } else {
+            freq=1; ans=nums[i];
+        }
+
+        if(freq > n/2){
+            cout  << "Majority element is: " << nums[i] << endl;
+            return 0;
+        }
+    }
 
     return 0;
 }
