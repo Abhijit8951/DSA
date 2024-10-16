cd02a4f15ad6beef3aca83b223c9db9c9de9a14c
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-16 08:14:52 +0530

    Added logic

--- container_most_water/Brute_force.cpp
+++ container_most_water/Brute_force.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 
 int main(){
+    int maxWater = 0;
+    vector<int> height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    for(int i=0;i<height.size();i++){
+        for(int j=i+1;j<height.size();j++){
+            int width=j-i;
+            int h=min(height[i], height[j]);
+            int area=width*h;
 
+            maxWater=max(maxWater, area);
+        }
+    }
+
+    cout << "Max water capacity is: " << maxWater << endl;
 
     return 0;
 }
