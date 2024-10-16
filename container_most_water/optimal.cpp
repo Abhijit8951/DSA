1ea46ed44ff1b7df9e758580aa319df799a70ff6
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-16 08:24:47 +0530

    Added logic

--- container_most_water/optimal.cpp
+++ container_most_water/optimal.cpp
@@ -3,7 +3,21 @@
 using namespace std;
 
 int main(){
+    vector<int> height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    int leftPoint=0, rightPoint=height.size()-1, maxWater=0;
+    while(leftPoint < rightPoint){
+        int w=rightPoint-leftPoint;
+        int h=min(height[leftPoint], height[rightPoint]);
+        int area=w*h;
+        maxWater=max(maxWater, area);
+        if(height[leftPoint] < height[rightPoint]){
+            leftPoint++;
+        } else{
+            rightPoint--;
+        }
+    }
 
+    cout << "Max water capacity is: " << maxWater << endl;
 
     return 0;
 }
