e65bba346ed0f04c4bfda1cf184e0236081fd110
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-17 07:32:23 +0530

    Added logic

--- product_array_except_self/optimal.cpp
+++ product_array_except_self/optimal.cpp
@@ -4,7 +4,26 @@
 using namespace std;
 
 int main(){
-    
+    vector<int> nums = {1, 2, 3, 4};
+    int n = nums.size();
+    vector<int> ans(n, 1);
+    //Forward
+    for(int i=1;i<n;i++){
+        ans[i] = ans[i-1] * nums[i-1];
+    }
 
-    rerturn 0;
+    //Reverse
+    int right =1;
+    for(int i=n-1;i>=0;i--){
+        ans[i] *= right;
+        right *= nums[i];
+    }
+
+    //Print Answer
+    for(int i=0;i<n;i++){
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
