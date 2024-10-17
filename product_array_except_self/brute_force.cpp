10e97c13c29178286a6be9e41fa1ddfd2a97c08d
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-17 06:25:52 +0530

    Added logic

--- product_array_except_self/brute_force.cpp
+++ product_array_except_self/brute_force.cpp
@@ -4,7 +4,20 @@
 using namespace std;
 
 int main(){
-
+    vector<int> nums = {1, 2, 3, 4}, ans;
+    int prod=1;
+    for(int  i=0;i<nums.size();i++){
+        prod=1;
+        for(int j=0;j<nums.size();j++){
+            if(i != j){
+                prod*=nums[j];
+            }
+        }
+        ans.push_back(prod);
+    }
+    for(int i=0;i<ans.size();i++){
+        cout << ans[i] << endl;
+    }
 
     return 0;
 }
