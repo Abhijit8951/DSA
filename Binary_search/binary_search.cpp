34ea2c56070c7d5c650d6ebfec797398842e6a5f
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-17 12:29:29 +0530

    Added logic

--- Binary_search/binary_search.cpp
+++ Binary_search/binary_search.cpp
@@ -3,8 +3,26 @@
 #include <vector>
 using namespace std;
 
-int main(){
+int binarysearch(vector<int> nums, int target){
+    int st=0, end=nums.size()-1;
 
+    while(st <= end){
+        int mid = st + (end - st)/2;
+        if(target < nums[mid]){
+            end = mid-1;
+        } else if(target > nums[mid]){
+            st = mid+1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector<int> nums = {0, 2, 4, 6, 8, 10};
+    int target=8;
+    cout << binarysearch(nums, target) << endl;
 
     return 0;
 }
