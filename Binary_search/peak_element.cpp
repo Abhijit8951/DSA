05444b976b9e597d279cf7a5461050cea01421ad
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-18 17:02:04 +0530

    Added logic

--- Binary_search/peak_element.cpp
+++ Binary_search/peak_element.cpp
@@ -4,7 +4,21 @@
 using namespace std;
 
 int main(){
-    
+    vector<int> nums = {0, 3, 8, 9, 5, 2};
+    int st=1, end=nums.size()-2;
+
+    while(st <= end){
+        int mid = st+(end - st)/2;
+        if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]){
+            cout << nums[mid] << endl;
+        }
+
+        if(nums[mid]<nums[mid+1]){
+            st=mid+1;
+        } else{
+            end=mid-1;
+        }
+    }
 
     return 0;
 }
