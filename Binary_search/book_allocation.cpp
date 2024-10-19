3389236fe35fb727f239e8ae58d8e12d92d5f499
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-19 20:27:36 +0530

    Added logic

--- Binary_search/book_allocation.cpp
+++ Binary_search/book_allocation.cpp
@@ -3,8 +3,55 @@
 #include <vector>
 using namespace std;
 
-int main(){
+bool isValid(vector<int> &arr,int n,int m,int maxallowedPages){
+    int student=1,pages=0; 
 
+    for(int i=0;i<n;i++){
+        if(arr[i]>maxallowedPages){
+            return false;
+        }
+
+        if(pages + arr[i] <= maxallowedPages){
+            pages += arr[i];
+        } else{
+            student++;
+            pages=arr[i];
+        }
+    }
+
+    return student > m ? false : true;
+}
+
+int allocateBooks(vector<int> &arr,int n,int m){
+    if(m > n){
+        return -1;
+    }
+
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum += arr[i];
+    }
+
+    int ans=-1;
+    int st=0, end=sum;
+    while(st <= end){
+        int mid = st+(end-st)/2;
+
+        if(isValid(arr, n, m, mid)){
+            ans=mid;
+            end=mid-1;
+        } else{
+            st=mid+1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr = {2, 1, 3, 4};
+    int n=4, m=2;
+
+    cout << allocateBooks(arr, n, m) << endl;
 
     return 0;
 }
