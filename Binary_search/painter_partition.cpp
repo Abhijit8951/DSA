293fd3cbda5d07aa9cb8f20d29f0da15a0a3e524
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-20 22:13:31 +0530

    Added logic

--- Binary_search/painter_partition.cpp
+++ Binary_search/painter_partition.cpp
@@ -3,8 +3,48 @@
 #include <vector>
 using namespace std;
 
-int main(){
+bool isPossible(vector<int> &arr, int n, int m, int MaxAllowedTime){
+    int painters=1, time=0;
 
+    for(int i=0;i<n;i++){
+        if(time + arr[i] <= MaxAllowedTime){
+            time += arr[i];
+        } else{
+            painters++;
+            time=arr[i];
+        }
+    }
+    return painters <= m;
+}
+
+int minTimeToPaint(vector<int> &arr, int n, int m){
+    int sum=0, maxVal=INT_MIN;
+    for(int i=0;i<n;i++){
+        sum += arr[i];
+        maxVal = max(maxVal, arr[i]);
+    }
+
+    int st=maxVal, end=sum, ans=-1;
+
+    while(st <= end){
+        int mid = st + (end-st)/2;
+
+        if(isPossible(arr, n, m, mid)){
+            ans=mid;
+            end=mid-1;
+        } else{
+            st=mid+1;
+        }
+    }
+
+    return ans;
+}
+
+int main(){
+    vector<int> arr = {40, 30, 10, 20};
+    int n=4, m=2;
+
+    cout << minTimeToPaint(arr,n,m) << endl;
 
     return 0;
 }
