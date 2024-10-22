0c4f091031097658af59955500198e441d5473e8
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-22 06:23:05 +0530

    added logic

--- sorting/bubble_sort/sort.cpp
+++ sorting/bubble_sort/sort.cpp
@@ -3,8 +3,33 @@
 #include <vector>
 using namespace std;
 
-int main(){
+void bubbleSort(int arr[], int n){
+    for(int i=0;i<n-1;i++){
+        bool isSwap = false;
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                isSwap = true;
+            }
+        }
+        if(!isSwap){
+            return;
+        }
+    }
+}
 
+void printArray(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    int n = 5;
+    int arr[] = {4, 1, 5, 2, 3};
+    bubbleSort(arr, n);
+    printArray(arr, n);
 
     return 0;
 }
