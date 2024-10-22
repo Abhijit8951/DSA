3033fcfeb9c2956f7696ba2c0e7e1df959490a5a
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-22 19:52:45 +0530

    added some edge cases

--- sorting/bubble_sort/sort.cpp
+++ sorting/bubble_sort/sort.cpp
@@ -7,7 +7,7 @@ void bubbleSort(int arr[], int n){
     for(int i=0;i<n-1;i++){
         bool isSwap = false;
         for(int j=0;j<n-i-1;j++){
-            if(arr[j]>arr[j+1]){
+            if(arr[j]>arr[j+1]){    // use if(arr[j]<arr[j+1]) in case of descending order
                 swap(arr[j], arr[j+1]);
                 isSwap = true;
             }
