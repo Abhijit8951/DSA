82e71afaeda2f679d06e412c12398b09a4db0b33
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-22 06:58:20 +0530

    Added logic

--- sorting/selection_sort/sort.cpp
+++ sorting/selection_sort/sort.cpp
@@ -3,8 +3,25 @@
 #include <vector>
 using namespace std;
 
-int main(){
+void selectionSort(int arr[], int n){
+    for(int i=0;i<n-1;i++){
+        int smallestIdx = i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j] < arr[smallestIdx]){
+                smallestIdx = j;
+            }
+        }
+        swap(arr[i], arr[smallestIdx]);
+    }
+}
 
+int main(){
+    int n = 5;
+    int arr[] = {4, 1, 5, 2, 3};
+    selectionSort(arr, n);
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
