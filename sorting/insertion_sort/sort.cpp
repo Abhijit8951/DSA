9eaa4d69f7b3233e5451bd73ee6fa319cf4de77d
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-22 19:48:20 +0530

    Added logic

--- sorting/insertion_sort/sort.cpp
+++ sorting/insertion_sort/sort.cpp
@@ -2,7 +2,29 @@
 #include <iostream>
 using namespace std;
 
+void insertionSort(int arr[], int n){
+    for(int i=1;i<n;i++){
+        int curr = arr[i];
+        int prev = i-1;
+
+        while(prev >= 0 && arr[prev] > curr){   // Use while(prev >= 0 && arr[prev] < curr) in case of descending order
+            arr[prev+1] = arr[prev];
+            prev--;
+        }
+
+        arr[prev+1] = curr;
+    }
+}
+
 int main(){
+    int n = 5;
+    int arr[] = {4, 1, 5, 2, 3};
+
+    insertionSort(arr, n);
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
 
     return 0;
