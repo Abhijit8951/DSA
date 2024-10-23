8e04e42ff93fa4d3766d278275c51abe63877e4c
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-23 06:35:38 +0530

    added initials

---
+++ sorting/sort_arrayof_0s_1s_2s/optimised.cpp
@@ -0,0 +1,11 @@
+//Sort an array with 0s, 1s, 2s using optimised approach
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[] = [2, 0, 2, 1, 1, 0, 1, 2, 0, 0];
+    int n = arr.size();
+
+
+    return 0;
+}
