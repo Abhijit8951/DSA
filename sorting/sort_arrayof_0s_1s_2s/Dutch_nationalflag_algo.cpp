40fad2e6e4ab81c7a83556d8f39018840122e3c5
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-23 06:55:48 +0530

    Added initials

---
+++ sorting/sort_arrayof_0s_1s_2s/Dutch_nationalflag_algo.cpp
@@ -0,0 +1,13 @@
+//Sort an array with 0s, 1s, 2s using Dutch National Flag  Algorithm
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> arr = {2, 0, 1, 1, 0, 1, 2, 0, 0};
+    int n = arr.size();
+
+
+
+    return 0;
+}
