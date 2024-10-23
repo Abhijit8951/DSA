c12458019b48fe235379961553972e81d7e2a7f5
Abhijit swain <swain.abhijit0119@gmail.com>
2024-10-23 06:56:28 +0530

    addded logic

--- sorting/sort_arrayof_0s_1s_2s/Dutch_nationalflag_algo.cpp
+++ sorting/sort_arrayof_0s_1s_2s/Dutch_nationalflag_algo.cpp
@@ -6,7 +6,24 @@ using namespace std;
 int main(){
     vector<int> arr = {2, 0, 1, 1, 0, 1, 2, 0, 0};
     int n = arr.size();
+    int mid=0, low=0, high=n-1;
 
+    while(mid <= high){
+        if(arr[mid] == 0){
+            swap(arr[low], arr[mid]);
+            mid++;
+            low++;
+        } else if(arr[mid] == 1){
+            mid++;
+        } else{
+            swap(arr[high], arr[mid]);
+            high--;
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
 
     return 0;
