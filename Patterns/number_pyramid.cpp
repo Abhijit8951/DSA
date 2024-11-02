0669611f5c66f75a9f44d1c8c3a28786216d0770
Abhijit swain <swain.abhijit0119@gmail.com>
2024-11-02 23:23:21 +0530

    Added number Pyramid

---
+++ Patterns/number_pyramid.cpp
@@ -0,0 +1,19 @@
+//Number Pyramid
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter tha number to print pyramid: ";
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n - i; ++j) {
+            cout << " ";
+        }
+        for (int k = 1; k <= i; ++k) {
+            cout << k << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
