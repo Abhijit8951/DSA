5a3849c17ffb937a5484473eca47fcdcfaf0e136
Abhijit swain <swain.abhijit0119@gmail.com>
2024-11-08 04:48:11 +0530

    Added linear search

---
+++ 2D-array/linear_search.cpp
@@ -0,0 +1,21 @@
+//Linear search in a 2D-Array
+#include <iostream>
+using namespace std;
+
+int main(){
+    int matrix[4][3] = {{1, 2, 3},{4, 5, 6,},{7, 8, 9},{10, 11, 12}};
+    int row=4;
+    int column=3;
+    int key=8;
+
+    for(int  i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            if(matrix[i][j] == key){
+                cout << "Key found in " << i << " , " << j << endl;
+            }
+        }
+    }
+
+
+    return 0;
+}
