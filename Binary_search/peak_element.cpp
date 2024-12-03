eed5e5acd1c03cf8e1ce415b78376c613fec1d4f
Abhijit swain <swain.abhijit0119@gmail.com>
2024-12-03 06:21:50 +0530

    Added some edge cases

--- Binary_search/peak_element.cpp
+++ Binary_search/peak_element.cpp
@@ -5,20 +5,26 @@ using namespace std;
 
 int main(){
     vector<int> nums = {0, 3, 8, 9, 5, 2};
-    int st=1, end=nums.size()-2;
+    int n = nums.size();
+    int st=0, end=n-1;
 
-    while(st <= end){
-        int mid = st+(end - st)/2;
-        if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]){
-            cout << nums[mid] << endl;
+    while (st <= end) {
+            int mid = st + (end - st) / 2;
+
+            // Check if mid is the peak element
+            bool left_smaller = (mid == 0) || (nums[mid] > nums[mid - 1]);
+            bool right_smaller = (mid == n - 1) || (nums[mid] > nums[mid + 1]);
+
+            if (left_smaller && right_smaller) {
+                cout <<  nums[mid] << endl;
+                return 0;
+            } 
+            else if (left_smaller && !right_smaller) {
+                st = mid + 1;
+            } 
+            else {
+                end = mid - 1;
+            }
         }
-
-        if(nums[mid]<nums[mid+1]){
-            st=mid+1;
-        } else{
-            end=mid-1;
-        }
-    }
-
-    return 0;
+        return -1;
 }
