d1628db1e7ca365a199c1c99a88beef300e84e81
Abhijit swain <swain.abhijit0119@gmail.com>
2025-01-02 06:07:19 +0530

    Added some edge cases

--- Arrays/find_in_mountain_array.cpp
+++ Arrays/find_in_mountain_array.cpp
@@ -17,30 +17,40 @@ int findPeakIndex(int low, int high, vector<int> mountain){
 }
 
 //searching for elements in both left & right side
-int binarySearch(int st, int end, int target, vector<int> mountain){
+int binarySearch(int st, int end, int target, vector<int> mountain, bool ascending){
     while(st <= end){
         int mid = st + (end-st) / 2;
 
-        if(mountain[mid] == target){
-            return mid;
-        } else if(target > mountain[mid]){
-            st = mid + 1;
-        } else{
-            end = mid - 1;
-        }
+            if(mountain[mid] == target) {
+                return mid;
+            }
+
+            if(ascending) { 
+                if (target > mountain[mid]) {
+                    st = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                if (target < mountain[mid]) {
+                    st = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
     }
     return -1;
 }
 
 int main(){
-    vector<int> mountain = {1, 2, 3, 4, 5, 3, 1};
-    int target = 3;
+    vector<int> mountain = {0, 5, 3, 1};
+    int target = 1;
     int n = mountain.size();
 
     int peakIndex = findPeakIndex(1, n - 2, mountain);
 
-    int incIndex = binarySearch(0, peakIndex, target, mountain);
-    int decIndex = binarySearch(peakIndex + 1, n - 1, target, mountain);
+    int incIndex = binarySearch(0, peakIndex, target, mountain, true);
+    int decIndex = binarySearch(peakIndex + 1, n - 1, target, mountain, false);
 
     if(incIndex == -1){
         cout << "Index: " << decIndex << endl;
