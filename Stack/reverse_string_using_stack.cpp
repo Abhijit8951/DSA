28b6d726bedf30a016f80d28ffa61012b6c38aa1
Abhijit swain <swain.abhijit0119@gmail.com>
2025-02-02 05:51:02 +0530

    Added reverse string using stack

---
+++ Stack/reverse_string_using_stack.cpp
@@ -0,0 +1,28 @@
+//Reverse a string using stack
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main() {
+    string str = "stack";
+
+    stack<char> s;
+
+    for(int i=0; i<str.length(); i++) {
+        char ch = str[i];
+        s.push(ch);
+    }
+
+    string ans = "";
+
+    while(!s.empty()) {
+        char ch = s.top();
+        ans.push_back(ch);
+        s.pop();
+    }
+
+    cout << "Reversed string is: " << ans << endl;
+
+
+    return 0;
+}
