31bafe6b0ba044eae9076ac2d12f31edd62a172b
Abhijit swain <swain.abhijit0119@gmail.com>
2025-02-26 11:32:58 +0530

    Added building tree from levelorder traversal

---
+++ Binary Tree/buildfromlevelorder.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class node {
+    public:
+        int data;
+        node* left;
+        node* right;
+
+    node(int d) {
+        this -> data = d;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+void levelOrderTraversal(node* root) {
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()) {
+        node* temp = q.front();
+        q.pop();
+
+        if(temp == NULL) { 
+            //purana level complete traverse ho chuka hai
+            cout << endl;
+            if(!q.empty()) { 
+                //queue still has some child ndoes
+                q.push(NULL);
+            }  
+        }
+        else{
+            cout << temp -> data << " ";
+            if(temp ->left) {
+                q.push(temp ->left);
+            }
+
+            if(temp ->right) {
+                q.push(temp ->right);
+            }
+        }
+    }
+
+}
+
+void buildFromLevelOrder(node* &root) {
+    queue<node*> q;
+
+    cout << "Enter data for root" << endl;
+    int data ;
+    cin >> data;
+    root = new node(data);
+    
+    q.push(root);
+
+    while(!q.empty()) {
+        node* temp = q.front();
+        q.pop();
+
+        cout << "Enter left node for: " << temp->data << endl;
+        int leftData;
+        cin >> leftData;
+
+        if(leftData != -1) {
+            temp -> left = new node(leftData);
+            q.push(temp->left);
+        }
+
+        cout << "Enter right node for: " << temp->data << endl;
+        int rightData;
+        cin >> rightData;
+
+        if(rightData != -1) {
+            temp -> right = new node(rightData);
+            q.push(temp->right);
+        }
+    }
+ }
+
+ int main() {
+    node* root = NULL;
+
+    buildFromLevelOrder(root);
+    levelOrderTraversal(root);
+
+
+    return 0;
+}
