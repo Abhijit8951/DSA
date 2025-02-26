2836d33a864d241c61c6ca4603bee64c9c2276c9
Abhijit swain <swain.abhijit0119@gmail.com>
2025-02-26 11:03:31 +0530

    Added tree preorder traversal

---
+++ Binary Tree/preorder_traversal.cpp
@@ -0,0 +1,56 @@
+// Preorder Traversal
+#include<iostream>
+using namespace std;
+
+class node {
+    public:
+        int data;
+        node* left;
+        node* right;
+
+    node(int d) {
+        this -> data = d;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+node* buildTree(node* root) {
+    cout << "Enter the data: " << endl;
+    int data;
+    cin >> data;
+    root = new node(data);
+
+    if(data == -1) {
+        return NULL;
+    }
+
+    cout << "Enter data for inserting in left of  " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter data for inserting in right of " << data << endl;
+    root->right = buildTree(root->right);
+    return root;
+
+}
+
+void preorder(node* root) {
+    if(root == NULL) {
+        return;
+    }
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main() {
+    node* root = NULL;
+
+    root = buildTree(root);
+
+    cout << "Preorder Traversal: " << endl;
+    preorder(root);
+
+
+    return 0;
+}
