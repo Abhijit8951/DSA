6473e2533f3c0ce9d6f94dd9b9ae95b38d856f5b
Abhijit swain <swain.abhijit0119@gmail.com>
2025-03-04 06:27:14 +0530

    Added symmetric tree

---
+++ Binary Tree/symmetric_tree.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+bool isMirror(TreeNode* left, TreeNode* right) {
+    if (!left && !right) return true;
+    if (!left || !right) return false;
+    return (left->val == right->val) && isMirror(left->left, right->right) && isMirror(left->right, right->left);
+}
+
+bool isSymmetric(TreeNode* root) {
+    if (!root) return true;
+    return isMirror(root->left, root->right);
+}
+
+int main() {
+   
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(2);
+    root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(4);
+    root->right->left = new TreeNode(4);
+    root->right->right = new TreeNode(3);
+
+    if (isSymmetric(root))
+        cout << "The tree is symmetric." << endl;
+    else
+        cout << "The tree is not symmetric." << endl;
+
+    return 0;
+}
