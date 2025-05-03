4c637c41219fe1d48c13152c94ee1f46d5c9dc44
Abhijit swain <swain.abhijit0119@gmail.com>
2025-05-04 02:16:05 +0530

    Added graph implementation

---
+++ Graph/implementation.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<unordered_map>
+#include<list>
+using namespace std;
+
+class graph {
+    public:
+        unordered_map<int, list<int>> adj;
+
+        void addEdge(int u, int v, bool direction) {
+            //direction = 0 -> undirected
+            //direction = 1 -> directed
+
+            adj[u].push_back(v);
+            if(direction == 0) {
+                adj[v].push_back(u);
+            }
+        }
+
+        void printAdjList() {
+            for(auto i : adj) {
+                cout << i.first << "->";
+                for(auto j : i.second) {
+                    cout << j << ", ";
+                }
+                cout << endl;
+            }
+        }
+};
+
+int main() {
+    int n;
+    cout << "Enter the number of nodes: ";
+    cin >> n;
+
+    int m;
+    cout << "Enter the number of edges: ";
+    cin >> m;
+
+    graph g;
+
+    for(int i=0; i<m; i++) {
+        int u, v;
+        cin >> u >> v;
+        g.addEdge(u, v, 0); // undirected graph
+    }
+
+    g.printAdjList();
+
+
+    return 0;
+}
